bb8da963b87f3abd60f513912260d76dde1139e9
Nithin <raonithin457@gmail.com>
2025-05-17 10:17:49 +0530

    Footer again

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,10 +18,10 @@ export default function Footer() {
             <Github className="h-5 w-5" />
             <span className="sr-only">GitHub</span>
           </Button>
-          <Link href="/about" variant="ghost" size="sm" className="rounded-full hover:bg-blue-500/5 hover:text-blue-500">
+          <Link href="/About" variant="ghost" size="sm" className="rounded-full hover:bg-blue-500/5 hover:text-blue-500">
             About
           </Link>
-          <Link href="/contact" variant="ghost" size="sm" className="rounded-full hover:bg-blue-500/5 hover:text-blue-500">
+          <Link href="/Contact" variant="ghost" size="sm" className="rounded-full hover:bg-blue-500/5 hover:text-blue-500">
             Contact
           </Link>
         </div>
